d57002a3c228bca2efe0666ad3d02b4732ca5e2e
assassinsilence <jnjam6681>
2023-10-30 22:55:58 +0700

    add command name

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 import { program } from 'commander';
 import initialize from './lib/common.ts';
 
+program.name('workshop-cli').description('Example command from deno');
+
 (await import('./lib/command/message.ts')).default(program);
 
 initialize(program);
