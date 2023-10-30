b45f96c23c36c6cd57ba3df17bd72e39b59e3541
assassinsilence <jnjam6681>
2023-10-30 23:01:27 +0700

    update config

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
 import { program } from 'commander';
 import initialize from './lib/common.ts';
 
-program.name('workshop-cli').description('Example command from deno');
+program
+  .configureHelp({
+    sortOptions: false,
+    showGlobalOptions: true,
+  })
+  .name('workshop-cli')
+  .description('Example command from deno');
 
 (await import('./lib/command/message.ts')).default(program);
 
