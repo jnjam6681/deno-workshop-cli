4f45924c9a8e26671e08ebfb41c9df20f3e4743f
assassinsilence <jnjam6681>
2023-10-30 23:11:57 +0700

    add config and version

---
+++ config/config.ts
@@ -0,0 +1,5 @@
+export const config = {
+    app: {
+        version: "1.0.0"
+    }
+}
--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { program } from 'commander';
 import initialize from './lib/common.ts';
+import { config } from "./config/config.ts";
 
 program
   .configureHelp({
@@ -7,6 +8,7 @@ program
     showGlobalOptions: true,
   })
   .name('workshop-cli')
+  .version(config.app.version)
   .description('Example command from deno');
 
 (await import('./lib/command/message.ts')).default(program);
